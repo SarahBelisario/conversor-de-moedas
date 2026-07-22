e9c5b84c67c7912b979c1c3108162ce574609225
Sarah Belisario <sarahhaide89@gmail.com>
2026-07-22 18:02:43 -0300

    Implementa captura do formulário e lógica de seleção das moedas.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,33 @@
+const USD = 5.07
+const EUR = 5.78
+const GBP = 6.77
+
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
 
 amount.addEventListener("input", () => {
 
     const hasCharactersRegex = /\D+/g
     amount.value = amount.value.replace(hasCharactersRegex, "")
 })
+
+form.onsubmit = (event) => {
+    event.preventDefault()
+
+    switch (currency.value){
+        case "USD":
+            convertCurrency(amount.value, USD, "US$")
+            break
+        case "EUR":
+            convertCurrency(amount.value, EUR, "€")
+            break
+        case "GBP":
+            convertCurrency(amount.value, GBP, "₤")
+            break
+    }
+}
+
+function convertCurrency(amount, price, symbol) {
+    console.log(amount, price, symbol)
+}
