621bce3d7cef6ea17270d803cdcc23441bc8b60f
Sarah Belisario <sarahhaide89@gmail.com>
2026-07-23 15:25:25 -0300

    Exibe a classe que exibe o footer para mostrar o resultado

--- scripts.js
+++ scripts.js
@@ -5,6 +5,7 @@ const GBP = 6.77
 const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
+const footer = document.querySelector("main footer")
 
 amount.addEventListener("input", () => {
 
@@ -29,5 +30,12 @@ form.onsubmit = (event) => {
 }
 
 function convertCurrency(amount, price, symbol) {
-    console.log(amount, price, symbol)
+    try {
+        footer.classList.add("show-result")
+    } catch (error) {
+        console.log(error)
+        footer.classList.remove("show-result")
+        alert("Não foi possivel converter. Tente novamente mais tarde.")
+    }
 }
+
